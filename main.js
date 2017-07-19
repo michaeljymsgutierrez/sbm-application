12b7bb4f46f4b6cdfe4bc221b8dc28cbfe2dc2d1
Chaelgutierrez <michael@vielsoft.com>
2017-07-19 13:19:21 +0800

    Added main js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Initialize electron application
+var electron = require('electron');
+// Initialize the main electron application 
+var { app } = electron;
+// Initialize the Browser window Form
+var { BrowserWindow } = electron;
+
+// Platform ready
+app.on('ready', function(){
+	// Initialize browser window
+	var win  = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
+
+	// Initialize main view
+	win.loadURL('file://' + __dirname + '/index.html');
+
+	// Load web console
+	win.webContents.openDevTools();
+
+});
+
