6983c9126c3cd05b478a3a60bc3226ed1ccdad3d
Chaelgutierrez <michael@vielsoft.com>
2017-07-20 15:34:26 +0800

    Added temporary routes for side menu

--- js/state.js
+++ js/state.js
@@ -1,1 +1,17 @@
+'use strict';
+
 // States and Configurations
+app.config(function($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+	.state('home',{
+		url: '/home',
+		templateUrl: './template/home/home.html'
+	})
+	.state('employee-home',{
+		url: '/employee-home',
+		templateUrl: './template/employee-management/employee-home.html'
+	});
+
+	$urlRouterProvider.otherwise('/home');
+});
