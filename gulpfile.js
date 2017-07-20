a20fe0ae9f47175fcc75c4d364ae8a14b15aa25a
Chaelgutierrez <michael@vielsoft.com>
2017-07-20 09:03:16 +0800

    Added gulp taks and watchers

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+
+var paths = {
+  sass: ['./scss/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/bms.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', ['sass'], function() {
+  gulp.watch(paths.sass, ['sass']);
+});
