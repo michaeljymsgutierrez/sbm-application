8139dbb97ca9ddd497ab78b1b3042427598dba79
Chaelgutierrez <michael@vielsoft.com>
2017-07-21 18:29:10 +0800

    Updated templates

--- js/script.js
+++ js/script.js
@@ -2,5 +2,19 @@
 
 // Load all jquery js codes 
 $(document).ready(function(){
+	
+	setInterval(function(){
+		// Add effects on circle buttons
+	  	$('.btn-circle').on('click',function(){
+	  		$(this).addClass('btn-dash');
+	  		setTimeout(function(){
+	  			$('.btn-circle').removeClass('btn-dash');
+	  		},100);
+	  	});
+	  	
+	},500);
+
+	// Assure that the side script has been loaded succesfully
 	console.log('Script has been loaded . . .');
 });
+
