4a159a60b2558b15d6c9912dc7de2dc1a331b01a
Chaelgutierrez <michael@vielsoft.com>
2017-07-22 10:24:11 +0800

    Updated directives

--- js/directive.js
+++ js/directive.js
@@ -3,25 +3,25 @@
 // Custom Directives 
 
 // Sidebar Menu
-app.directive('sideMenuBar',function(){
-	return {
-		restrict: 'E',
-		templateUrl: './template/directive-template/side-menu-bar.html'
-	}
+app.directive('sideMenuBar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: './template/directive-template/side-menu-bar.html'
+    }
 });
 
 // One space
-app.directive('spO',function(){
-	return {
-		restrict: 'E',
-		template: '&nbsp;'
-	}
+app.directive('spO', function() {
+    return {
+        restrict: 'E',
+        template: '&nbsp;'
+    }
 });
 
 // Two space
-app.directive('spT',function(){
-	return {
-		restrict: 'E',
-		template: '&nbsp;&nbsp;'
-	}
+app.directive('spT', function() {
+    return {
+        restrict: 'E',
+        template: '&nbsp;&nbsp;'
+    }
 });
