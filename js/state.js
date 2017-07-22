0f299f3be52402fc1417c6c1d7bbd3c3eb6ba14e
Chaelgutierrez <michael@vielsoft.com>
2017-07-22 10:31:26 +0800

    Added state route for settings section

--- js/state.js
+++ js/state.js
@@ -1,17 +1,22 @@
 'use strict';
 
 // States and Configurations
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider) {
 
-	$stateProvider
-	.state('dashboard',{
-		url: '/dashboard',
-		templateUrl: './template/dashboard/dashboard.html'
-	})
-	.state('employee-home',{
-		url: '/employee-home',
-		templateUrl: './template/employee-management/employee-home.html'
-	});
+    $stateProvider
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: './template/dashboard/dashboard.html'
+        })
+        .state('employee-home', {
+            url: '/employee-home',
+            templateUrl: './template/employee-management/employee-home.html'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: './template/settings/settings.html'
+        });
 
-	$urlRouterProvider.otherwise('/dashboard');
+    // $urlRouterProvider.otherwise('/dashboard');
+    $urlRouterProvider.otherwise('/settings');
 });
