ff65a531989622a779404a848e2e9363853cc776
Chaelgutierrez <michael@vielsoft.com>
2017-07-24 14:06:17 +0800

    Updated main js

--- main.js
+++ main.js
@@ -8,20 +8,19 @@ var { app } = electron;
 var { BrowserWindow } = electron;
 
 // Platform ready
-app.on('ready', function(){
-	// Initialize browser window and set screen size to full screen
-	var { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
-  	var win = new BrowserWindow({ 
-  		width, 
-  		height,
-  		icon: __dirname + "/img/icon.png" 
-  	});
+app.on('ready', function() {
+    // Initialize browser window and set screen size to full screen
+    var { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+    var win = new BrowserWindow({
+        width,
+        height,
+        icon: __dirname + "/img/icon.png"
+    });
 
-	// Initialize main view
-	win.loadURL('file://' + __dirname + '/index.html');
+    // Initialize main view
+    win.loadURL('file://' + __dirname + '/index.html');
 
-	// Load web console
-	// win.webContents.openDevTools();
+    // Load web console
+    // win.webContents.openDevTools();
 
 });
-
