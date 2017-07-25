acded79436c123431ae543bcd910a3d0dcf6cda9
Chaelgutierrez <michael@vielsoft.com>
2017-07-25 16:56:51 +0800

    added controller for initial setup

--- js/state.js
+++ js/state.js
@@ -6,7 +6,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('setup', {
             url: '/setup',
-            templateUrl: './template/initial-setup/initial-setup.html'
+            templateUrl: './template/initial-setup/initial-setup.html',
+            controller: 'setupCtrl'
         })
         .state('dashboard', {
             url: '/dashboard',
