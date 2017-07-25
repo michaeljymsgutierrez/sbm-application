9cc4fb675d2172b5f34a24391d12659841c23c2c
Chaelgutierrez <michael@vielsoft.com>
2017-07-25 12:11:29 +0800

    Added body listener

--- js/script.js
+++ js/script.js
@@ -1,37 +1,34 @@
 'use strict';
 
 jQuery(document).ready(function() {
-    // $(document).ready(function() {
-
-    // Assure that the side script has been loaded succesfully
+    /* Assure that the side script has been loaded succesfully */
     console.log('Script has been loaded . . .');
 
-    $(window).load(function() {
-        // Insert all your misc codes inside this block
-        setInterval(function() {
-            // Add effects on circle buttons
-            $('.btn-circle').on('click', function() {
-                $(this).addClass('btn-dash');
-                setTimeout(function() {
-                    $('.btn-circle').removeClass('btn-dash');
-                }, 100);
-            });
+    /* Put all your codes here */
+    $("body").on('DOMSubtreeModified', ".right-container", function() {
+        console.log("Reinitialize code . . .");
 
-            // Event listener for adding class on selected on side menu
-            $('a.menu-link').on('click', function() {
-                $('a.menu-link').removeClass('active-menu');
-                $(this).addClass('active-menu');
-            });
+        /* Add effects on circle buttons */
+        $('.btn-circle').on('click', function() {
+            $(this).addClass('btn-dash');
+            setTimeout(function() {
+                $('.btn-circle').removeClass('btn-dash');
+            }, 100);
+        });
 
+        /*  Event listener for adding class on selected on side menu */
+        $('a.menu-link').on('click', function() {
+            $('a.menu-link').removeClass('active-menu');
+            $(this).addClass('active-menu');
+        });
 
-            // Add effects on button click
-            $('.btn-animate').on('click', function() {
-                $('.btn-animate').addClass('btn-animate-click');
-                setTimeout(function() {
-                    $('.btn-animate').removeClass('btn-animate-click')
-                }, 300);
-            });
+        /* Add effects on button click */
+        $('.btn-animate').on('click', function() {
+            $('.btn-animate').addClass('btn-animate-click');
+            setTimeout(function() {
+                $('.btn-animate').removeClass('btn-animate-click')
+            }, 600);
+        });
 
-        }, 500);
     });
 });
