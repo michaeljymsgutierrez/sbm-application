4f137f7d87bb3f208054a6aae52060bef044d99c
Chaelgutierrez <michael@vielsoft.com>
2017-07-25 16:59:40 +0800

    Added angular resource module

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 'use strict';
 
-//  Main application module
-var app = angular.module('bms',['ui.router']);
+/*  Main application module */
+var app = angular.module('bms', ['ui.router', 'ngResource']);
+
+
+/* Put all your configuration here */
+app.run(function($state) {
+
+
+});
