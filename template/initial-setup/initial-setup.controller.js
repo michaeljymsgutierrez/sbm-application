f88376bea81205401c25dfee0a50852d07f6ce3b
Chaelgutierrez <michael@vielsoft.com>
2017-07-26 10:32:38 +0800

    Added alert for validating api

--- template/initial-setup/initial-setup.controller.js
+++ template/initial-setup/initial-setup.controller.js
@@ -6,15 +6,18 @@ app.controller('setupCtrl', function($scope, validateApi, $q, storage) {
 
     /* API initialization */
     $scope.api = "";
-    $scope.data = "";
+    /* Alert visibility */
+    $scope.showAlert = false;
 
     $scope.validateURL = function() {
         var deferred = $q.defer();
         $scope.checkAPI = validateApi.connect($scope.api);
         $scope.checkAPI.get(function(data) {
+            $scope.showAlert = false;
             deferred.resolve(data);
             return deferred.promise;
         }, function(err) {
+            $scope.showAlert = true;
             deferred.reject(data);
             return deferred.promise;
         });
