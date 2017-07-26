ee9e9b8b57234fceeee5bb3b768b8ca4e26bb9a1
Chaelgutierrez <michael@vielsoft.com>
2017-07-26 15:59:58 +0800

    Injected backdrop service

--- template/initial-setup/initial-setup.controller.js
+++ template/initial-setup/initial-setup.controller.js
@@ -2,7 +2,7 @@
 
 /* Initial Setup controller */
 
-app.controller('setupCtrl', function($scope, validateApi, $q, storage, $resource) {
+app.controller('setupCtrl', function($scope, validateApi, storage, $state, backdrop, $timeout) {
 
     /* API initialization */
     $scope.api = "";
@@ -22,13 +22,16 @@ app.controller('setupCtrl', function($scope, validateApi, $q, storage, $resource
 
     /* Fn for validating api url */
     $scope.validateURL = function() {
+        backdrop.show();
         $scope.checkAPI = validateApi.connect($scope.api);
         $scope.checkAPI.get(function(res) {
+            backdrop.hide();
             storage.write('endpoint', $scope.api);
             $scope.showAlert = false;
             $scope.form1 = false;
             $scope.form2 = true;
         }, function(err) {
+            backdrop.hide();
             $scope.showAlert = true;
             $scope.form1 = true;
         });
@@ -36,26 +39,36 @@ app.controller('setupCtrl', function($scope, validateApi, $q, storage, $resource
 
     /* Fn for validatin api key */
     $scope.validateKey = function() {
+        backdrop.show();
         $scope.api = storage.read('endpoint') + "/store/branch";
         $scope.checkAPI = validateApi.key($scope.api, $scope.key);
         $scope.checkAPI.get(function(res) {
+            backdrop.hide();
             storage.write('apiKey', $scope.key);
             $scope.storelist = res.data;
             $scope.showAlert = false;
             $scope.form2 = false;
             $scope.form3 = true;
         }, function(err) {
+            backdrop.hide();
             $scope.showAlert = true;
         });
     };
 
     /* Fn for saving store */
     $scope.saveStore = function() {
+        backdrop.show();
         if ($scope.selectedStore == "" || $scope.selectedStore == undefined) {
+            backdrop.hide();
             $scope.showAlert = true;
         } else {
             $scope.showAlert = false;
             storage.write('store_id', $scope.selectedStore);
+            storage.write('reload', 'true');
+            $timeout(function() {
+                backdrop.hide();
+                $state.go('dashboard');
+            }, 1500);
         }
     }
 });
