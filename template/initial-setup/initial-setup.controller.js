2c9cc68ae1b7ebaa4d224d54f78229a36262eee5
Chaelgutierrez <michael@vielsoft.com>
2017-07-26 12:56:56 +0800

    Added storelist holder for form 3

--- template/initial-setup/initial-setup.controller.js
+++ template/initial-setup/initial-setup.controller.js
@@ -7,20 +7,21 @@ app.controller('setupCtrl', function($scope, validateApi, $q, storage, $resource
     /* API initialization */
     $scope.api = "";
     $scope.key = "";
-
     /* Alert visibility */
     $scope.showAlert = false;
-    $scope.showAlert2 = false;
-
     /* Form 1 API URL */
     $scope.form1 = true;
     /* Form 2 API KEY */
     $scope.form2 = false;
+    /* Form 3 STORE */
+    $scope.form3 = false;
+    /* Store List */
+    $scope.storelist = [];
 
     /* Fn for validating api url */
     $scope.validateURL = function() {
         $scope.checkAPI = validateApi.connect($scope.api);
-        $scope.checkAPI.get(function(data) {
+        $scope.checkAPI.get(function(res) {
             storage.write('endpoint', $scope.api);
             $scope.showAlert = false;
             $scope.form1 = false;
@@ -35,11 +36,13 @@ app.controller('setupCtrl', function($scope, validateApi, $q, storage, $resource
     $scope.validateKey = function() {
         $scope.api = storage.read('endpoint') + "/store/branch";
         $scope.checkAPI = validateApi.key($scope.api, $scope.key);
-        $scope.checkAPI.get(function(data) {
-            $scope.showAlert2 = false;
+        $scope.checkAPI.get(function(res) {
+            storage.write('apiKey', $scope.key);
+            $scope.storelist = res.data;
+            $scope.showAlert = false;
             $scope.form1 = false;
         }, function(err) {
-            $scope.showAlert2 = true;
+            $scope.showAlert = true;
         });
     };
 });
