f7f142740ae46b1e76dcb6679faad0f80989258e
Chaelgutierrez <michael@vielsoft.com>
2017-07-27 11:29:17 +0800

    Added model for settings template

--- template/settings/settings.controller.js
+++ template/settings/settings.controller.js
@@ -3,9 +3,14 @@
 
 /* Settings Controller */
 
-app.controller('settingsCtrl', function($scope, storage) {
-    console.log('settings controller . . .');
-    $scope.data = "TEST";
+app.controller('settingsCtrl', function($scope, storage, Toast) {
+
     /* Initialize variables */
     $scope.settings = {};
+
+    /* Fn for save settings */
+    $scope.saveSettings = function() {
+        Toast.show('Settings successfully save . . .');
+        storage.write('settings', $scope.settings);
+    };
 });
