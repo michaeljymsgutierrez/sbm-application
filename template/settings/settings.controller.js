c1d3c7d5b78f74eea1ba2ab02519876c43aebc6c
Chaelgutierrez <michael@vielsoft.com>
2017-07-27 10:22:22 +0800

    Test commit

--- template/settings/settings.controller.js
+++ template/settings/settings.controller.js
@@ -5,7 +5,7 @@
 
 app.controller('settingsCtrl', function($scope, storage) {
     console.log('settings controller . . .');
-
+    $scope.data = "SAMPLE DATA";
     /* Initialize variables */
     $scope.settings = {};
 });
