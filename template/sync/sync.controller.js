807336e60185851fcf0c2b9561c579512aaa0c7e
Chaelgutierrez <michael@vielsoft.com>
2017-07-28 13:49:47 +0800

    Added store sync info fn

--- template/sync/sync.controller.js
+++ template/sync/sync.controller.js
@@ -2,6 +2,18 @@
 
 /* Sync Controller */
 
-app.controller('syncCtrl', function($scope) {
+app.controller('syncCtrl', function($scope, storage, Store) {
+    /* Get store id */
+    var store_id = storage.read('store_id').store_id;
+
+    /* Sync Store  */
+    $scope.syncStore = function() {
+        Store.get({ id: store_id }, function(res) {
+            console.log(res);
+        }, function(err) {
+            console.log(err);
+        });
+    }
+
 
 });
