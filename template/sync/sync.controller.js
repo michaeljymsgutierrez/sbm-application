61c907edb647facbe03ab7bdaec893f4ca66e4d6
Chaelgutierrez <michael@vielsoft.com>
2017-07-31 13:18:03 +0800

    Updated store info api

--- template/sync/sync.controller.js
+++ template/sync/sync.controller.js
@@ -11,7 +11,7 @@ app.controller('syncCtrl', function($scope, storage, Store, DBAccess) {
 
         Store.get({ id: store_id }, function(res) {
             var response = res;
-            DBAccess.execute('SELECT COUNT(*) as count FROM store_info').then(function(res) {
+            DBAccess.execute('SELECT COUNT(*) as count FROM store_info', []).then(function(res) {
                 if (res[0].count == 0) {
                     var insert = "INSERT INTO store_info (store_id, store_code, store_name, company, location, bank) VALUES (?,?,?,?,?,?)";
                     var param = [response.store_id, response.store_code, response.store_name, response.company, response.location, response.bank.join()];
