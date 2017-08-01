4d1716ff082724f7b512c5df560d68f4a76d7e43
Chaelgutierrez <michael@vielsoft.com>
2017-08-01 13:36:45 +0800

    Added dateformatter service

--- js/angular-service.js
+++ js/angular-service.js
@@ -61,6 +61,52 @@ app.service('backdrop', function($timeout) {
 });
 
 
+/* Service for formatting date */
+app.service('dateFormatter', function($filter) {
+    /* month */
+    this.month = function(dt) {
+        return $filter('date')(dt, 'MMM');
+    }
+
+    /* date */
+    this.date = function(dt) {
+        return $filter('date')(dt, 'dd');
+    }
+
+    /* year */
+    this.year = function(dt) {
+        return $filter('date')(dt, 'yyyy');
+    }
+
+    /* standard format */
+    this.standard = function(dt) {
+        return $filter('date')(dt, 'yyyy-MM-dd HH:mm:ss');
+    }
+
+    /* standard utc */
+    this.utc = function(dt) {
+        var raw_date = new Date(dt);
+        var date_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+        return $filter('date')(date_utc, 'yyyy-MM-dd HH:mm:ss');
+    }
+
+    /* standard format no time */
+    this.standardNoTime = function(dt) {
+        return $filter('date')(dt, 'yyyy-MM-dd');
+    }
+
+    /* string format */
+    this.stringDate = function(dt) {
+        return $filter('date')(dt, 'MMMM d, y');
+    }
+
+    /* convert datetime to timestamp */
+    this.timestamp = function(dt) {
+        return Math.floor(new Date(dt).getTime() / 1000);
+    }
+});
+
+
 /* Service for Executing DB Queries */
 app.service('DBAccess', function($q) {
     /* Param must be array */
