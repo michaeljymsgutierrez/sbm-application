eb6f47eee88b5ba0629cf6f9705c4834c5f6efbc
Chaelgutierrez <michael@vielsoft.com>
2017-08-18 18:15:45 +0800

    Updated attendance controller js

--- template/employee-management/attendance.controller.js
+++ template/employee-management/attendance.controller.js
@@ -2,15 +2,34 @@
 
 /* Attendance Controller */
 
-app.controller('attendanceCtrl', function($scope) {
+app.controller('attendanceCtrl', function($scope, Modal, ModalService, $q, $element) {
 
     /* Function for Taking Photo */
     $scope.takePhoto = function() {
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
                 var src = window.URL.createObjectURL(stream);
+                var video = document.getElementById('video');
+                video.src = src;
             });
         }
+
+        $scope.closeMe = function() {
+            Modal.hide();
+        };
+
+        $scope.capture = function() {
+            console.log("Ready to capture");
+        };
+
+        ModalService.showModal({
+            templateUrl: "./template/employee-management/take-photo.html",
+            controller: "attendanceCtrl",
+            scope: $scope
+        }).then(function(modal) {
+            modal.element.modal();
+        });
+
     };
 
 });
