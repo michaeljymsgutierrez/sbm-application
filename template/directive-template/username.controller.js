ced9a7e1fc8a631bc973138600cb5ce7e1f08e12
Chaelgutierrez <michael@vielsoft.com>
2017-08-23 10:22:28 +0800

    Added user info broadcaster from parent

--- template/directive-template/username.controller.js
+++ template/directive-template/username.controller.js
@@ -2,30 +2,36 @@
 
 /* Username Controller */
 
-app.controller('usernameCtrl', function($scope, Toast, Modal, $state, $timeout, DBAccess, Log) {
+app.controller('usernameCtrl', function($rootScope, $scope, Toast, Modal, $state, $timeout, DBAccess, Log) {
+
+    /* Function for redirecting to dashbard */
+    function gotoDashboard() {
+        Modal.hide();
+        Toast.show('Employee not found');
+        $timeout(function() {
+            $state.go('dashboard');
+        }, 1000);
+    }
 
     /* Validation for username and Event Listener Across DOM */
     $timeout(function() {
         jQuery('.user-prompt .cancel').click(function() {
-            Modal.hide();
-            Toast.show('Employee not found');
-            $timeout(function() {
-                $state.go('dashboard');
-            }, 1000);
+            gotoDashboard();
         });
 
         jQuery('.user-prompt .submit').click(function() {
             $scope.username = jQuery('.username-input').val();
             if ($scope.username == undefined | $scope.username == '') {
-                Modal.hide();
-                Toast.show('Employee not found');
-                $timeout(function() {
-                    $state.go('dashboard');
-                }, 1000);
+                gotoDashboard();
             } else {
-                var query = 'SELECT * FROM employee WHERE username = ?';
+                var query = 'SELECT * FROM employee WHERE username = ? AND active = 1';
                 DBAccess.execute(query, [$scope.username]).then(function(res) {
-                    console.log(res);
+                    if (res.length > 0) {
+                        Modal.hide();
+                        $rootScope.$broadcast('user', res[0]);
+                    } else {
+                        gotoDashboard();
+                    }
                 }, function(err) {
                     Log.write(err);
                 });
