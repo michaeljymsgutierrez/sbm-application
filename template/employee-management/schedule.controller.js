f3912b315a710ae6619da2afdb29f4133c7935c6
Chaelgutierrez <michael@vielsoft.com>
2017-08-23 11:32:06 +0800

    Added  function for displaying user schedule

--- template/employee-management/schedule.controller.js
+++ template/employee-management/schedule.controller.js
@@ -2,7 +2,24 @@
 
 /* Schedule Controller */
 
-app.controller('scheduleCtrl', function(Username) {
+app.controller('scheduleCtrl', function(Username, $rootScope, $scope, DBAccess, Log, $filter) {
 
     Username.popup();
+
+    /* Listen for broadcasted user information */
+    $rootScope.$on('user', function(event, data) {
+        $scope.name = data.name;
+        var eid = data.employee_id;
+        var query = 'SELECT es.employee_id, es.date, es.shift, (SELECT store_name FROM branch_info WHERE _id = es.branch_id ) as branch FROM employee_schedule es WHERE es.employee_id = ?';
+        DBAccess.execute(query, [eid]).then(function(res) {
+            angular.forEach(res, function(value) {
+                var start = $filter('date')(new Date(value.shift.split('|')[0]), 'h:mm a');
+                var end = $filter('date')(new Date(value.shift.split('|')[1]), 'h:mm a');
+                value.shift = start + " - " + end;
+            });
+            $scope.schedule = res;
+        }, function(err) {
+            Log.write(err);
+        });
+    });
 });
