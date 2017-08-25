a37eebf36c348a518d244c35f55e875a0d451a4d
Chaelgutierrez <michael@vielsoft.com>
2017-08-25 08:55:26 +0800

    Added compress task for build

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,35 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var pump = require('pump');
+var uglify = require('gulp-uglify');
 
 var paths = {
-  sass: ['./scss/*.scss']
+    sass: ['./scss/*.scss']
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/bms.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./css/'))
-    .on('end', done);
+    gulp.src('./scss/bms.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./css/'))
+        .on('end', done);
 });
 
 gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('compress', function(cb) {
+    pump([gulp.src(['js/*.js', 'template/*.js']),
+        uglify(),
+        gulp.dest('production-dist')
+    ]);
 });
