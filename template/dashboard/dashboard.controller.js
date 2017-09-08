339e0a394a65f623d2c45cb79394a6c097881ccd
Chaelgutierrez <michael@vielsoft.com>
2017-09-08 10:32:23 +0800

    Added auto storename set when not store not synced yet

--- template/dashboard/dashboard.controller.js
+++ template/dashboard/dashboard.controller.js
@@ -4,11 +4,15 @@
 
 app.controller('dashboardCtrl', ['DBAccess', '$scope', function(DBAccess, $scope) {
 
-    /* Get store name for dashboard */
+    /* 
+        Get store name for dashboard 
+        In case of error Dashboard will automatically set as storename
+    */
+    $scope.store_name = 'Dashboard';
     var storeInfo = "SELECT store_name FROM store_info";
     DBAccess.execute(storeInfo, []).then(function(res) {
         if (res.length == 0) {
-            $scope.store_name = 'Dashboard'
+            $scope.store_name = 'Dashboard';
         } else {
             $scope.store_name = res[0].store_name;
         }
