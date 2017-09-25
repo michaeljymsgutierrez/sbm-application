8fb9f66d878fda65c2b5c698b8c10ac5adb41792
Chaelgutierrez <michael@vielsoft.com>
2017-09-25 16:21:02 +0800

    Updated numpad controller

--- template/directive-template/number-pad.controller.js
+++ template/directive-template/number-pad.controller.js
@@ -2,6 +2,45 @@
 
 /* Number Pad Controller */
 
-app.controller('numberPadCtrl', ['$scope', function($scope) {
-    $scope.data = 'sample data';
+app.controller('numberPadCtrl', ['$scope', '$rootScope', 'Modal', 'DBAccess', 'Log', 'Toast', function($scope, $rootScope, Modal, DBAccess, Log, Toast) {
+    /*
+        Initialize output as numpad numeric output
+    */
+    $scope.output = "";
+
+    /* Function for updating scope output */
+    $scope.updateOutput = function(key) {
+        $scope.output = $scope.output.concat(key);
+    };
+
+    /* Functions for numpad actions */
+    $scope.delete = function() {
+        $scope.output = $scope.output.slice(0, -1);
+    };
+    $scope.clear = function() {
+        $scope.output = "";
+    };
+    $scope.cancel = function() {
+        Modal.hide();
+    };
+    $scope.ok = function() {
+        if ($scope.output != "") {
+            if ($rootScope.numpad_sender == "inventory-waste") {
+                var id = $rootScope.item.id;
+                var query = "SELECT qty FROM inventory_actual WHERE created = (SELECT max(created) FROM inventory_actual) AND inventory_id = ?";
+                DBAccess.execute(query, [id]).then(function(res) {
+                    if (parseInt($scope.output) > parseInt(res[0].qty)) {
+                        Toast.show("Requested quantity is greater than item stack quantity");
+                    } else if ($scope.output == 0) {
+                        Toast.show("Requested quantity must be greater than 0");
+                    } else {
+                        $rootScope.$broadcast('numpad:input', $scope.output);
+                    }
+                }, function(err) {
+                    Log.write(err);
+                });
+            }
+        }
+    };
+
 }]);
