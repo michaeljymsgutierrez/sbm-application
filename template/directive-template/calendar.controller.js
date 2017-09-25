0411a13149c7533d3cbc185b79d42f8c277f1a17
Chaelgutierrez <michael@vielsoft.com>
2017-09-25 11:22:33 +0800

    Added selected date broadcast

--- template/directive-template/calendar.controller.js
+++ template/directive-template/calendar.controller.js
@@ -2,13 +2,24 @@
 
 /* Calendar Controller */
 
-app.controller('calendarCtrl', ['$rootScope', '$scope', 'dateFormatter', function($rootScope, $scope, dateFormatter) {
+app.controller('calendarCtrl', ['$rootScope', '$scope', 'dateFormatter', 'Modal', function($rootScope, $scope, dateFormatter, Modal) {
 
     /*
         Function for fetching selected date from calendar
     */
+    $rootScope.$broadcast('date-picker', dateFormatter.standardNoTime(new Date($scope.datePicker)));
     $rootScope.dateChange = function() {
-        $rootScope.selecteDate = dateFormatter.standardNoTime(new Date($scope.datePicker));
+        $rootScope.$broadcast('date-picker', dateFormatter.standardNoTime(new Date($scope.datePicker)));
+    };
+
+    /*
+        Function for calendar OK and CANCEL
+    */
+    $scope.calendarOk = function() {
+        Modal.hide();
+    };
+    $scope.calendarCancel = function() {
+        Modal.hide();
     };
 
 }]);
