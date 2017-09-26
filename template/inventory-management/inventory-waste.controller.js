c1f02e6eaa28628e9ecdc61f49f3b21f1daf049e
Chaelgutierrez <michael@vielsoft.com>
2017-09-26 14:58:00 +0800

    Added scope for category and pdated waste menu category styling

--- template/inventory-management/inventory-waste.controller.js
+++ template/inventory-management/inventory-waste.controller.js
@@ -2,12 +2,26 @@
 
 /* Inventory Waste Controller */
 
-app.controller('inventoryWasteCtrl', ['$scope', 'DBAccess', 'Username', '$rootScope', function($scope, DBAccess, Username, $rootScope) {
+app.controller('inventoryWasteCtrl', ['$scope', 'DBAccess', 'Username', '$rootScope', 'Log', function($scope, DBAccess, Username, $rootScope, Log) {
 
     Username.popup();
     var unregisterUser = $rootScope.$on('user', function(event, data) {
         unregisterUser();
         $scope.user = data;
+        $scope.inventory_display = [];
+        $scope.category = ['All'];
+
+        var query = "SELECT id, _id, name, uom, category_name FROM inventory WHERE status = 1";
+        DBAccess.execute(query, []).then(function(res) {
+            /* Contain inventory item */
+            $scope.inventory_display = res;
+            angular.forEach($scope.inventory_display, function(value) {
+                $scope.category.push(value.category_name);
+            });
+
+        }, function(err) {
+            Log.write(err);
+        });
     });
 
 }]);
