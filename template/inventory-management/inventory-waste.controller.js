8b5e7b128cefc42c907a50f3c22649a7047b539a
Chaelgutierrez <michael@vielsoft.com>
2017-09-26 11:18:44 +0800

    Added intial styling for inventory waste

--- template/inventory-management/inventory-waste.controller.js
+++ template/inventory-management/inventory-waste.controller.js
@@ -2,6 +2,12 @@
 
 /* Inventory Waste Controller */
 
-app.controller('inventoryWasteCtrl', ['$scope', function($scope) {
+app.controller('inventoryWasteCtrl', ['$scope', 'DBAccess', 'Username', '$rootScope', function($scope, DBAccess, Username, $rootScope) {
+
+    Username.popup();
+    var unregisterUser = $rootScope.$on('user', function(event, data) {
+        unregisterUser();
+        $scope.user = data;
+    });
 
 }]);
