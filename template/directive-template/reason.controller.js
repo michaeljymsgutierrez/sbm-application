7aaea63cdc0fa69a0f6143c5e50bff07e0a80e65
Chaelgutierrez <michael@vielsoft.com>
2017-09-26 10:32:36 +0800

    Added reason broadcaster for inventory waste module

--- template/directive-template/reason.controller.js
+++ template/directive-template/reason.controller.js
@@ -2,13 +2,14 @@
 
 /* Reason Controller */
 
-app.controller('reasonCtrl', ['$scope', 'DBAccess', '$rootScope', 'Log', function($scope, DBAccess, $rootScope, Log) {
+app.controller('reasonCtrl', ['$scope', 'DBAccess', '$rootScope', 'Log', 'Modal', 'Toast', function($scope, DBAccess, $rootScope, Log, Modal, Toast) {
 
     /*
         Reason to be displayed must be checked whos sender
         check sender by logging rootScope reason_sender to display corresponding reason module
     */
     $scope.reason = [];
+    $scope.selected_reason = "";
     if ($rootScope.reason_sender == "inventory-waste") {
         var query = "SELECT reason FROM reason WHERE module = 'inventory_wastage'";
         DBAccess.execute(query, []).then(function(res) {
@@ -21,5 +22,22 @@ app.controller('reasonCtrl', ['$scope', 'DBAccess', '$rootScope', 'Log', functio
             Log.write(err);
         });
     }
+
+    /*
+        Reason button functions
+    */
+    $scope.cancel = function() {
+        Modal.hide();
+    };
+    $scope.select = function() {
+        if ($scope.selected_reason != "") {
+            if ($rootScope.reason_sender == "inventory-waste") {
+                $rootScope.$broadcast('reason:inventory-waste', $scope.selected_reason);
+                Modal.hide();
+            }
+        } else {
+            Toast.show("Reason is required");
+        }
+    };
 
 }]);
