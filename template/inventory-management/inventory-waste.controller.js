76988374f2f8e82c843672b00d29faaf1dcc85c4
Chaelgutierrez <michael@vielsoft.com>
2017-09-26 15:54:02 +0800

    Added auto category filter

--- template/inventory-management/inventory-waste.controller.js
+++ template/inventory-management/inventory-waste.controller.js
@@ -14,7 +14,9 @@ app.controller('inventoryWasteCtrl', ['$scope', 'DBAccess', 'Username', '$rootSc
         var query = "SELECT id, _id, name, uom, category_name FROM inventory WHERE status = 1";
         DBAccess.execute(query, []).then(function(res) {
             /* Contain inventory item */
+            $scope.inventory_item = res;
             $scope.inventory_display = res;
+            /* Load category from database */
             angular.forEach($scope.inventory_display, function(value) {
                 $scope.category.push(value.category_name);
             });
@@ -22,5 +24,21 @@ app.controller('inventoryWasteCtrl', ['$scope', 'DBAccess', 'Username', '$rootSc
             Log.write(err);
         });
     });
+
+    /*
+        Function for category filtering of item on click
+    */
+    $scope.filterByCategory = function(category) {
+        $scope.inventory_display = [];
+        if (category == 'All') {
+            $scope.inventory_display = $scope.inventory_item;
+        } else {
+            angular.forEach($scope.inventory_item, function(value) {
+                if (value.category_name == category) {
+                    $scope.inventory_display.push(value);
+                }
+            });
+        }
+    };
 
 }]);
