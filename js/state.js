fe890a8556927f5cf935736630822c6b7f572134
Chaelgutierrez <michael@vielsoft.com>
2017-09-27 13:59:55 +0800

    Added state route for warehouse

--- js/state.js
+++ js/state.js
@@ -47,6 +47,11 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
             templateUrl: './template/inventory-management/inventory/inventory-waste.html',
             controller: 'inventoryWasteCtrl'
         })
+        .state('warehouse-records', {
+            url: '/warehouse-records',
+            templateUrl: './template/inventory-management/warehouse/warehouse-records.html',
+            controller: ''
+        })
         .state('settings', {
             url: '/settings',
             templateUrl: './template/settings/settings.html',
