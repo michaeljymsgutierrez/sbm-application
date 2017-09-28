1d0c0b182da179eb7fb4028e34d5341e5d19edfd
Chaelgutierrez <michael@vielsoft.com>
2017-09-28 15:35:21 +0800

    Added initial display for category and warehouse item

--- template/inventory-management/warehouse/warehouse-order.controller.js
+++ template/inventory-management/warehouse/warehouse-order.controller.js
@@ -2,11 +2,32 @@
 
 /* Warehouse order Controller */
 
-app.controller('warehouseOrderCtrl', ['$scope', 'Username', '$rootScope', function($scope, Username, $rootScope) {
+app.controller('warehouseOrderCtrl', ['$scope', 'Username', '$rootScope', 'DBAccess', function($scope, Username, $rootScope, DBAccess) {
     Username.popup();
+
+    var initWarehouse = function() {
+        $scope.warehouse_display = [];
+        $scope.category = ['All'];
+        var query = "SELECT id, _id, name, uom, category_name FROM inventory WHERE status = 1";
+        DBAccess.execute(query, []).then(function(res) {
+            /* Contain Warehouse Item */
+            $scope.warehouse_item = res;
+            $scope.warehouse_display = res;
+
+            /* Load category */
+            angular.forEach($scope.warehouse_item, function(value) {
+                if ($scope.category.indexOf(value.category_name) == -1) {
+                    $scope.category.push(value.category_name);
+                }
+            });
+        }, function(err) {
+            Log.write(err);
+        });
+    };
 
     var unregisterUser = $rootScope.$on('user', function(event, data) {
         unregisterUser();
         $scope.user = data;
+        initWarehouse();
     });
 }]);
