7baea0eab06d84bc8c54a8ca80bcd41e88c9d5ec
Chaelgutierrez <michael@vielsoft.com>
2017-09-29 15:17:37 +0800

    Added three way binding from root to child

--- template/directive-template/number-pad.controller.js
+++ template/directive-template/number-pad.controller.js
@@ -22,6 +22,10 @@ app.controller('numberPadCtrl', ['$scope', '$rootScope', 'Modal', 'DBAccess', 'L
         if ($rootScope.item.qty) {
             $scope.output = $rootScope.item.qty.toString();
         }
+    } else if ($rootScope.numpad_sender == "warehouse-order") {
+        if ($rootScope.item.qty) {
+            $scope.output = $rootScope.item.qty.toString();
+        }
     }
 
     /* Functions for numpad actions */
