b8dcff2982ee21492f572d3961b4b05622a69a46
Chaelgutierrez <michael@vielsoft.com>
2017-10-04 10:22:29 +0800

    Added initial warehouse records display

--- template/inventory-management/warehouse/warehouse-records.controller.js
+++ template/inventory-management/warehouse/warehouse-records.controller.js
@@ -2,7 +2,20 @@
 
 /* Warehouse records controller */
 
-app.controller('warehouseRecordsCtrl', ['$scope', 'Username', function($scope, Username) {
+app.controller('warehouseRecordsCtrl', ['$scope', 'Username', 'DBAccess', '$rootScope', 'Log', function($scope, Username, DBAccess, $rootScope, Log) {
     Username.popup();
-    $scope.data = "SAMPLE DATA";
+
+    var unregisteUser = $rootScope.$on('user', function(event, data) {
+        unregisteUser();
+        $scope.user = data;
+        /* Warehouse records main container */
+        $scope.warehouse_records = [];
+        var query = "SELECT wt.id, wt.type, wt.transaction_number,(SELECT name FROM employee WHERE id = wt.created_by) AS name, status, created FROM warehouse_transaction wt";
+        DBAccess.execute(query, []).then(function(res) {
+            console.log(res);
+            $scope.warehouse_records = res;
+        }, function() {
+            Log.write(err);
+        });
+    });
 }]);
