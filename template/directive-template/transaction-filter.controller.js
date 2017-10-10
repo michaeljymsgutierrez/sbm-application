35a117803d2ce917736f72b3da34fca07aa264df
Chaelgutierrez <michael@vielsoft.com>
2017-10-10 09:08:34 +0800

    Added modal service , cancel, select fn transaction filter

--- template/directive-template/transaction-filter.controller.js
+++ template/directive-template/transaction-filter.controller.js
@@ -4,12 +4,23 @@
     Transction Filter controller
 */
 
-app.controller('transactionFilterCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+app.controller('transactionFilterCtrl', ['$scope', '$rootScope', 'Toast', 'Modal', function($scope, $rootScope, Toast, Modal) {
 
     /*
         Function for broadcasting Transaction Type
     */
-    $scope.selectedTransactionType = function() {
-        $rootScope.$broadcast('filter:transaction-type', $scope.transactio_type);
+    $scope.select = function() {
+        if ($scope.transaction_type == undefined) {
+            $scope.transaction_type = 'all';
+        }
+        $rootScope.$broadcast('filter:transaction-type', $scope.transaction_type);
+        Modal.hide();
+    };
+
+    /*
+        Function for cancel transaction type modal
+    */
+    $scope.cancel = function() {
+        Modal.hide();
     };
 }]);
