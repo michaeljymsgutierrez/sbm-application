2867207a6cc28e5fecac87243eab3306cf8e2a44
Chaelgutierrez <michael@vielsoft.com>
2017-10-17 14:43:27 +0800

    Added validation for warehouse transaction statuses

--- template/inventory-management/warehouse/warehouse-delivery.controller.js
+++ template/inventory-management/warehouse/warehouse-delivery.controller.js
@@ -4,7 +4,7 @@
     Warehouse Delivery Controller
 */
 
-app.controller('warehouseDeliveryCtrl', ['$scope', 'DBAccess', 'Username', '$rootScope', 'Toast', function($scope, DBAccess, Username, $rootScope, Toast) {
+app.controller('warehouseDeliveryCtrl', ['$scope', 'DBAccess', 'Username', '$rootScope', 'Toast', 'Log', function($scope, DBAccess, Username, $rootScope, Toast, Log) {
 
     Username.popup();
     /*
@@ -21,7 +21,18 @@ app.controller('warehouseDeliveryCtrl', ['$scope', 'DBAccess', 'Username', '$roo
         */
         $scope.searchTransaction = function() {
             if ($scope.order_no != "") {
-
+                var query = "SELECT * FROM warehouse_transaction WHERE transaction_number = ?";
+                DBAccess.execute(query, [$scope.order_no]).then(function(res) {
+                    if (res[0].is_synced == 0) {
+                        Toast.show("Warehouse transaction is unsynced");
+                    } else if (res[0].status == 0) {
+                        Toast.show("Warehouse transaction is waiting for approval");
+                    } else {
+                        console.log(res);
+                    }
+                }, function(err) {
+                    Log.write(err);
+                });
             } else {
                 Toast.show("Please input order no");
             }
