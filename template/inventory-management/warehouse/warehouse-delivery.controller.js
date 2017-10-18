43bf4804c8328df66dac05e09ca83fed65de86e1
Chaelgutierrez <michael@vielsoft.com>
2017-10-18 10:55:09 +0800

    Added tid for order delivery items

--- template/inventory-management/warehouse/warehouse-delivery.controller.js
+++ template/inventory-management/warehouse/warehouse-delivery.controller.js
@@ -30,11 +30,11 @@ app.controller('warehouseDeliveryCtrl', ['$scope', 'DBAccess', 'Username', '$roo
                         } else if (res[0].status == 0) {
                             Toast.show("Warehouse transaction is waiting for approval");
                         } else {
+                            /* Transaction ID for order delivery items */
                             var tid = res[0].id;
                             var query = "SELECT wr.item_id, wr.quantity, wr.approved_quantity, (SELECT name FROM inventory WHERE id = wr.item_id) AS item, (SELECT uom FROM inventory WHERE id = wr.item_id) AS uom FROM warehouse_request wr WHERE wr.transaction_id = ?";
                             DBAccess.execute(query, [tid]).then(function(res) {
                                 $scope.order_delivery_item = res;
-                                console.log(res);
                             }, function(err) {
                                 Log.write(err);
                             });
