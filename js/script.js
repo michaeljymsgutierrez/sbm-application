50d11c41bd8f7ec4c85be2598efdc9be3bd758e4
Chaelgutierrez <michael@vielsoft.com>
2017-10-24 08:42:03 +0800

    Refactor script cycle - reduce app load

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@
 function scriptCycle() {
 
     /* Include all your jQuery code here */
-    jQuery(document).ready(function() {
+    jQuery(window).load(function() {
 
         /* Assure that the side script has been loaded succesfully */
         console.log('Script running on background mode . . .');
 
+        var body = jQuery('body');
+
         /* Add effects on circle buttons */
-        $('.btn-circle').on('click', function() {
+        body.on('click', '.btn-circle', function() {
             $(this).addClass('btn-dash');
             setTimeout(function() {
                 $('.btn-circle').removeClass('btn-dash');
@@ -21,53 +23,35 @@ function scriptCycle() {
         });
 
         /*  Event listener for adding class on selected on side menu */
-        $('a.menu-link').on('click', function() {
+        body.on('click', 'a.menu-link', function() {
             $('a.menu-link').removeClass('active-menu');
             $(this).addClass('active-menu');
         });
 
-        /*  Event listener for adding class on selected on active tab */
-        // $('div.tab').click(function() { });
 
         /* Attendance Form Button event listener */
-        $('.attendance-form button').click(function() {
+        body.on('click', '.attendance-form button', function() {
             $(this).css('background-color', 'black');
             setTimeout(function() {
                 $('.attendance-form button').css('background-color', '#444');
             }, 100);
         });
 
-        /* Resize Left and Right Container on inital setup */
-        if (window.location.hash == '#!/setup') {
-            $('div.left-container').remove();
-            $('div.right-container').css('width', '100vw');
-        }
-
-        /* Reload browser window temporarily */
-        if (JSON.parse(window.localStorage.getItem('reload')) == 'true') {
-            window.localStorage.removeItem('reload');
-            window.localStorage.setItem('default', 'dashboard');
-            location.reload();
-        }
-
-        /* Turn of click event for modal */
-        $('.modal').off('click');
 
         /* Rotate Sync Effect */
-        $('.sync-btn').on('click', function() {
+        body.on('click', '.sync-btn', function() {
             var element = $(this);
             element.addClass('rotate');
         });
 
-        /* Remove rotate class all over DOM element if backdrop is hidden */
-        if ($('.custom-backdrop').css('visibility') == 'hidden') {
-            $('.sync-btn').removeClass('rotate');
-        }
 
-        /* Adjusted loader margin on setup container */
-        if ($('div.setup').length != 0) {
-            $('.loader').css({ 'margin-left': '47.5vw' });
-        }
+        /* Add active class for category */
+        body.on('click', 'button.category', function() {
+            var element = $(this);
+            $('button.category').removeClass('active-category');
+            element.addClass('active-category');
+        });
+
 
         /* Remove drag element and select */
         $('*').attr('ondragstart', 'return false');
@@ -75,14 +59,40 @@ function scriptCycle() {
         $('input').attr('ondragstart', 'return true');
         $('input').attr('onselectstart', 'return true');
 
-        /* Add active class for category */
-        $('button.category').click(function() {
-            var element = $(this);
-            $('button.category').removeClass('active-category');
-            element.addClass('active-category');
-        });
+        /*
+            Add here all non event listener
+        */
+        setInterval(function() {
+
+            /* Resize Left and Right Container on inital setup */
+            if (window.location.hash == '#!/setup') {
+                $('div.left-container').remove();
+                $('div.right-container').css('width', '100vw');
+            }
+
+            /* Reload browser window temporarily */
+            if (JSON.parse(window.localStorage.getItem('reload')) == 'true') {
+                window.localStorage.removeItem('reload');
+                window.localStorage.setItem('default', 'dashboard');
+                location.reload();
+            }
+
+            /* Remove rotate class all over DOM element if backdrop is hidden */
+            if ($('.custom-backdrop').css('visibility') == 'hidden') {
+                $('.sync-btn').removeClass('rotate');
+            }
+
+            /* Adjusted loader margin on setup container */
+            if ($('div.setup').length != 0) {
+                $('.loader').css({ 'margin-left': '47.5vw' });
+            }
+
+            /* Turn off click event for modal */
+            $('.modal').off('click');
+
+        }, 100);
 
     });
 }
 
-setInterval(scriptCycle, 1000);
+scriptCycle();
