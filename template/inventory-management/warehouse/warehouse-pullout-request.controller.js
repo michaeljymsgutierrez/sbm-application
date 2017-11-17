ff18ca64394f7f8b15ad4522cb5064d3094cd368
Chaelgutierrez <michael@vielsoft.com>
2017-11-17 09:54:38 +0800

    Added fns init warehouse data,  filter by category - warehouse pullout request

--- template/inventory-management/warehouse/warehouse-pullout-request.controller.js
+++ template/inventory-management/warehouse/warehouse-pullout-request.controller.js
@@ -3,6 +3,54 @@
 /*
     Pullout Request Controller
 */
-app.controller('pulloutRequestCtrl', ['$scope', function($scope) {
-    $scope.data = "SAMPLE DATA";
+app.controller('pulloutRequestCtrl', ['$scope', 'Username', '$rootScope', 'DBAccess', 'dateFormatter', 'storage', 'NumberPad', 'Log', 'Toast', function($scope, Username, $rootScope, DBAccess, dateFormatter, storage, NumberPad, Log, Toast) {
+    Username.popup();
+
+    /* 
+        Initialize Warehouse Item 
+    */
+    var initWarehouse = function() {
+        $scope.warehouse_display = [];
+        $scope.category = ['All'];
+        $scope.datenow = dateFormatter.slashFormat(new Date());
+        $scope.employee_name = $scope.user.name;
+        $scope.transaction_no = storage.read('store_code') + "-WO-" + dateFormatter.timestamp(new Date());
+        var query = "SELECT id, _id, name, uom, category_name FROM inventory WHERE status = 1";
+        DBAccess.execute(query, []).then(function(res) {
+            /* Contain Warehouse Item */
+            $scope.warehouse_item = res;
+            $scope.warehouse_display = res;
+
+            /* Load category */
+            angular.forEach($scope.warehouse_item, function(value) {
+                if ($scope.category.indexOf(value.category_name) == -1) {
+                    $scope.category.push(value.category_name);
+                }
+            });
+        }, function(err) {
+            Log.write(err);
+        });
+    };
+
+    /*
+        Function for Filterin Warehous Item by category
+    */
+    $scope.filterByCategory = function(x) {
+        $scope.warehouse_display = [];
+        if (x == 'All') {
+            $scope.warehouse_display = $scope.warehouse_item;
+        } else {
+            angular.forEach($scope.warehouse_item, function(value) {
+                if (x == value.category_name) {
+                    $scope.warehouse_display.push(value);
+                }
+            });
+        }
+    };
+
+    var unregisterUser = $rootScope.$on('user', function(event, data) {
+        unregisterUser();
+        $scope.user = data;
+        initWarehouse();
+    });
 }]);
